75d18551631884e70f969b20c43a5d8f6ef7644f
Jack Dawson <jack@fortelinea.com>
2016-10-29 20:06:21 -0700

    Add failing login tests

--- YouTrack.Tests/LoginTests.cs
+++ YouTrack.Tests/LoginTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using YouTrack.Models;
@@ -11,28 +9,37 @@ using YouTrack.Web;
 
 namespace YouTrack.Tests
 {
-    public class MockResponseHandler : DelegatingHandler
-    {
-        private readonly Dictionary<Uri, HttpResponseMessage> _mockReposonses =
-            new Dictionary<Uri, HttpResponseMessage>();
-
-        public void AddFakeResponse(Uri uri, HttpResponseMessage responseMessage)
-        {
-            _mockReposonses.Add(uri, responseMessage);
-        }
-
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
-            CancellationToken cancellationToken)
-        {
-            if (_mockReposonses.ContainsKey(request.RequestUri))
-                return await Task.FromResult(_mockReposonses[request.RequestUri]);
-            return new HttpResponseMessage(HttpStatusCode.NotFound) {RequestMessage = request};
-        }
-    }
-
     [TestFixture]
     public class LoginTests
     {
+        private const string BaseAddress = "http://test.com/";
+
+        [Test]
+        public async Task TestFailedLogin()
+        {
+            var mockReponseHandler = new MockResponseHandler();
+
+            var uri = new Uri($"{BaseAddress}{YoutrackDirectory.Authentication}");
+
+            mockReponseHandler.AddFakeResponse(uri, new HttpResponseMessage(HttpStatusCode.Unauthorized));
+
+            var clientFactory = new ClientFactory(mockReponseHandler);
+
+            var client = clientFactory.Create(BaseAddress);
+
+            var loginClient = new YouTrackClient(client);
+
+            var user = new User
+            {
+                Login = "testUser",
+                Password = "password".ToSecureString()
+            };
+
+            var success = await loginClient.AuthenticateAsync(user);
+
+            Assert.IsFalse(success);
+        }
+
         [Test]
         public async Task TestLogin()
         {
@@ -40,8 +47,7 @@ namespace YouTrack.Tests
 
             var stringContent = new StringContent("<login>ok</login>");
 
-            var baseAddress = "http://test.com/";
-            var uri = new Uri($"{baseAddress}{YoutrackDirectory.Authentication}");
+            var uri = new Uri($"{BaseAddress}{YoutrackDirectory.Authentication}");
 
             mockReponseHandler.AddFakeResponse(uri, new HttpResponseMessage(HttpStatusCode.Accepted)
             {
@@ -50,9 +56,7 @@ namespace YouTrack.Tests
 
             var clientFactory = new ClientFactory(mockReponseHandler);
 
-
-            var client = clientFactory.Create(baseAddress);
-
+            var client = clientFactory.Create(BaseAddress);
 
             var loginClient = new YouTrackClient(client);
 
---
+++ YouTrack.Tests/MockResponseHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YouTrack.Tests
+{
+    public class MockResponseHandler : DelegatingHandler
+    {
+        private readonly Dictionary<Uri, HttpResponseMessage> _mockReposonses =
+            new Dictionary<Uri, HttpResponseMessage>();
+
+        public void AddFakeResponse(Uri uri, HttpResponseMessage responseMessage)
+        {
+            _mockReposonses.Add(uri, responseMessage);
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            if (_mockReposonses.ContainsKey(request.RequestUri))
+                return await Task.FromResult(_mockReposonses[request.RequestUri]);
+            return new HttpResponseMessage(HttpStatusCode.NotFound) {RequestMessage = request};
+        }
+    }
+}
